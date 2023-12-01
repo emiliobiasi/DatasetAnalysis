e476569d5db0baaeeb2e827901fb5e0a43e3b089
unknown <ejbiasi@hotmail.com>
2023-12-01 13:14:34 -0300

    att7

--- analise2.py
+++ analise2.py
@@ -6,30 +6,21 @@ import scipy.stats as stats
 # Leitura do CSV
 dataframe = pd.read_csv(r'C:/PROJECTS/TAREFAS FACUL/DatasetAnalysis/vgsales.csv')
 print(dataframe)
-colunas_para_transformar = ['Other_Sales']
-dataframe[colunas_para_transformar] = dataframe[colunas_para_transformar] / 1_000_000
 
-def gera_tabela(df):
-    publishers = set(df['Publisher'])
-    tabela = []
-    for publisher in publishers:
-        jogos = []
-        sim = 0
-        nao = 0
-        for index, jogo in df.iterrows():
-            if jogo['Publisher'] == publisher:
-                df.drop(index=index)
-                jogos.append(jogo)
-        index=0
-        for jogo in jogos:
-            if index != 0:
-                if jogo['Global_Sales'] <= jogos[index-1]['Global_Sales']:
-                    nao += 1
-                else:
-                    sim += 1
-            index+=1
-        tabela.append([publisher, sim, nao, sim+nao])
-    print(tabela)
+# Filtrar os jogos da série GTA
+gta_games = dataframe[dataframe['Name'].str.contains('Grand Theft Auto', case=False)]
 
+# Selecionar apenas as colunas desejadas
+gta_games_subset = gta_games[['Name', 'Year', 'Global_Sales']]
 
-gera_tabela(dataframe)
+# Exibir os jogos da série GTA com as colunas selecionadas
+print(gta_games_subset)
+
+# Especificar o caminho do arquivo Excel de saída
+output_excel_path = 'C:/PROJECTS/TAREFAS FACUL/DatasetAnalysis/gta_games_subset.xlsx'
+
+# Salvar o DataFrame gta_games_subset em um arquivo Excel
+gta_games_subset.to_excel(output_excel_path, index=False)
+
+# Exibir mensagem informando que o Excel foi salvo com sucesso
+print(f'Dados dos jogos da série GTA salvos em: {output_excel_path}')
